585c5b0e85c5301d3935a618ad296f272480fc1e
unknown <kimyt0402@gmail.com>
2025-04-15 20:16:04 +0900

    [UPDATE] 상태 관리 간소화 및 불필요한 상태 분별

--- src/components/Log.jsx
+++ src/components/Log.jsx
@@ -1,7 +1,15 @@
 import { useState } from "react";
 
-export default function Log() {
+export default function Log({ turns }) {
   const [] = useState(0);
 
-  return <ol id="log"></ol>;
+  return (
+    <ol id="log">
+      {turns.map((turn) => (
+        <li key={`${turn.square.row}${turn.square.col}`}>
+          {turn.player} selected {turn.square.row}, {turn.square.col}
+        </li>
+      ))}
+    </ol>
+  );
 }
