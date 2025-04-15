b018cb1a1d9d86fee7040c3a2fd528903a50ab04
unknown <kimyt0402@gmail.com>
2025-04-15 20:41:21 +0900

    [UPDATE] 우승자 선별 방법 추가
    계산된 값에서 새로운 계산된 값 파생하기

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,12 @@ import Log from "./components/Log";
 import { useState } from "react";
 import { WINNING_COMBINATIONS } from "./winnig-combinations";
 
+const initialGameBoard = [
+  [null, null, null],
+  [null, null, null],
+  [null, null, null],
+];
+
 function deriveActivePlayer(gameTurns) {
   let currentPlayer = "X";
 
@@ -16,6 +22,32 @@ function deriveActivePlayer(gameTurns) {
 function App() {
   const [gameTurns, setGameTurns] = useState([]);
   const activePlayer = deriveActivePlayer(gameTurns);
+
+  let gameBoard = initialGameBoard;
+  let winner = null;
+
+  for (const turn of gameTurns) {
+    const { square, player } = turn;
+    const { row, col } = square;
+    gameBoard[row][col] = player;
+  }
+
+  for (const combination of WINNING_COMBINATIONS) {
+    const firstSquareSymbol =
+      gameBoard[combination[0].row][combination[0].column];
+    const secondSquareSymbol =
+      gameBoard[combination[1].row][combination[1].column];
+    const thirdSquareSymbol =
+      gameBoard[combination[2].row][combination[2].column];
+
+    if (
+      firstSquareSymbol &&
+      firstSquareSymbol === secondSquareSymbol &&
+      firstSquareSymbol === thirdSquareSymbol
+    ) {
+      winner = firstSquareSymbol;
+    }
+  }
 
   function handleSelectSquare(rowIndex, colIndex) {
     setGameTurns((prevTurns) => {
@@ -46,7 +78,8 @@ function App() {
               isActive={activePlayer === "O"}
             />
           </ol>
-          <GameBoard onSelectSquare={handleSelectSquare} turns={gameTurns} />
+          {winner && <p>You won, {winner}!</p>}
+          <GameBoard onSelectSquare={handleSelectSquare} board={gameBoard} />
         </div>
         <Log turns={gameTurns} />
       </main>
