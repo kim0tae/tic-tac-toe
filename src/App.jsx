cb51e44775cc8dc58540797373f0895f2a1cc06d
unknown <kimyt0402@gmail.com>
2025-04-15 20:26:44 +0900

    [FEATURE] 게임 승리 조건 추가

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Player from "./components/Player";
 import GameBoard from "./components/GameBoard";
 import Log from "./components/Log";
 import { useState } from "react";
+import { WINNING_COMBINATIONS } from "./winnig-combinations";
 
 function deriveActivePlayer(gameTurns) {
   let currentPlayer = "X";
